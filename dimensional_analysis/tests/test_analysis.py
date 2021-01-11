f9796b0e5aed5989ab837fb87d1eb207f2da479f
Christoph Heindl <christoph.heindl@gmail.com>
2021-01-11 09:57:57 +0100

    added test for solver

--- dimensional_analysis/tests/test_analysis.py
+++ dimensional_analysis/tests/test_analysis.py
@@ -1,4 +1,5 @@
 import pytest
+from functools import reduce
 import numpy as np
 from numpy.testing import assert_allclose
 
@@ -49,32 +50,31 @@ def test_matrix_system_meta(vs_example_72):
 
 def test_matrix_A_B_E(vs_example_72):
     dm = an.dimensional_matrix(*vs_example_72)
-    #m = np.array([[1,2,3,4,5],[2,4,3,0,2],[3,4,3,2,1]])
     A, B = an.matrix_A(dm), an.matrix_B(dm)
     assert_allclose(A, [[3,4,5],[3,0,2],[3,2,1]])
     assert_allclose(B, [[1,2],[2,4],[3,4]])
     E = an.matrix_E(A,B)
     assert_allclose(E*30, [[30,0,0,0,0],[0,30,0,0,0],[-32,-48,-4,6,8],[-6,6,3,-12,9],[18,12,6,6,-12]])
 
-def test_nonsingular_A(vs_example_72, vs_example_78):
+def test_ensure_nonsingular_A(vs_example_72, vs_example_78):
     dm = an.dimensional_matrix(*vs_example_72)
     q = si.unity
     dm_meta = an.DimensionalSystemMeta(dm, q)
-    del_row, col_order = an.nonsingular_A(dm, dm_meta)
+    del_row, col_order = an.ensure_nonsingular_A(dm, dm_meta)
     assert len(del_row) == 0
     assert_allclose(col_order,range(dm_meta.n_v))
 
     dm = an.dimensional_matrix(*vs_example_78)
     q = si.unity
     dm_meta = an.DimensionalSystemMeta(dm, q)
-    del_row, col_order = an.nonsingular_A(dm, dm_meta)
+    del_row, col_order = an.ensure_nonsingular_A(dm, dm_meta)
     assert len(del_row) == 1
     assert del_row[0] in [1,2]
     assert_allclose(col_order,range(dm_meta.n_v))
 
     dm = an.dimensional_matrix(si.M, si.L, si.L)
     dm_meta = an.DimensionalSystemMeta(dm, si.unity)
-    del_row, col_order = an.nonsingular_A(dm, dm_meta)
+    del_row, col_order = an.ensure_nonsingular_A(dm, dm_meta)
     assert len(del_row) == 1
     assert del_row[0] == 2
     assert col_order in [
@@ -83,5 +83,15 @@ def test_nonsingular_A(vs_example_72, vs_example_78):
         (2,0,1),
         (2,1,0)]
 
-# def test_solve():
-#     solve(L,L)
+def assert_dimensions(P, invars, q):
+    s = [reduce(
+            lambda prev, z: prev*z[0]**z[1], 
+            zip(invars, p), 
+            si.unity) for p in P]
+    assert_allclose(s, np.tile(np.asarray(q).reshape(1,-1), (P.shape[0],1)))
+
+def test_solve(vs_example_72):
+    q = si.L**3*si.M**5*si.T**7
+    P = an.solve(vs_example_72, q)
+    assert P.shape == (3,5)
+    assert_dimensions(P, vs_example_72, q)
