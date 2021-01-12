2273e36b43ccd0ca75955630405b089108100af4
Christoph Heindl <christoph.heindl@gmail.com>
2021-01-12 21:22:52 +0100

    improved docs

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -6,12 +6,13 @@ def main():
     import danalysis.standard_units as si
 
     r = da.solve(
-        {'a':si.M, 'b':si.F, 'c':si.T}, 
-        si.L
+        {'a':si.M, 'b':si.F, 'c':si.T, 'd':si.pressure}, 
+        si.L*si.T
     )
     print(r)
-    # Found 1 variable products generating dimension L:
-    #   1: [a**-1*b*c**2] = L
+    # Found 2 variable products generating dimension L*T:
+    #    1: [a*c**-1*d**-1] = L*T
+    #    2: [b**0.5*c*d**-0.5] = L*T
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
