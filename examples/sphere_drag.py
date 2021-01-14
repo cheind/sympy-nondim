35eb8d37db50939230b83ddc505965eaaf40fc36
Christoph Heindl <christoph.heindl@gmail.com>
2021-01-14 18:49:51 +0100

    initial code for drag-example

--- examples/sphere_drag.py
+++ examples/sphere_drag.py
@@ -4,15 +4,15 @@ import logging
 def main():
     import danalysis as da
 
-    si = da.standard_systems.SI         # predefined standard units
-    with da.new_problem() as p:         # records variables and dimensions
+    si = da.standard_systems.SI         
+    with da.new_problem() as p:         
         p.drag = si.F
-        p.mu = si.Viscosity             # order is important it seems.
+        p.mu = si.DynamicViscosity     
         p.b = si.L
         p.V = si.Velocity
         p.rho = si.Density
         
-        result = p.solve_for(si.Unity) # solve with target dimension
+        result = p.solve_for(si.Unity) 
         print(result)
         
 if __name__ == '__main__':
