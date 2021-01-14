f9d37b1a96d9ab01e95607a64fe7c7033e127b90
Christoph Heindl <christoph.heindl@gmail.com>
2021-01-14 10:53:37 +0100

    added derived dims

--- danalysis/standard_systems.py
+++ danalysis/standard_systems.py
@@ -3,9 +3,12 @@ from .quantities import DimensionalSystem
 def _add_LMT_derived_quantities(dimsys: DimensionalSystem) -> None:
     L,M,T = dimsys.L, dimsys.M, dimsys.T
     dimsys.F = L*M/T**2
+    dimsys.Velocity = L/T
+    dimsys.Acceleration = dimsys.Velocity/T
     dimsys.Pressure = dimsys.F/L**2
     dimsys.Density = M/L**3
     dimsys.Torque = L**2*M*T**-2
+    dimsys.Viscosity = M*(L*T)**-1
     dimsys.Angle = dimsys.Unity
 
 """International System of Units (SI)"""
